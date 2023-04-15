7742999c5a821eae1325b1edfc4cd21e9919790a
JavierDelAguila <113706141+JavierDelAguila@users.noreply.github.com>
2023-04-15 16:51:05 +0200

    Main Actualizado usando funciones UART y Coche

--- CochePolicia.X/PFinal.c
+++ CochePolicia.X/PFinal.c
@@ -1,65 +1,7 @@
 #include <xc.h>
 #include "Pic32Ini.h"
-
-
-#define rueda_izq 6
-#define rueda_der 9
-
-void avanzar(void)
-{
-    encenderCoche();
-    LATCCLR= (1<<rueda_izq | 1<<rueda_der); 
-    
-    //TO DO - ENCENDER 2 LEDS BLANCOS PARA MARCAR QUE VA MARCHA ATRAS
-}
-
-
-void marchaAtras(void)
-{
-    encenderCoche();
-    LATCSET= (1<<rueda_izq | 1<<rueda_der);
-    
-    //TO DO - ENCENDER 2 LEDS ROJOS PARA MARCAR QUE VA MARCHA ATRAS
-}
-
-void girarIzq(void)
-{
-    LATCCLR= (1<<rueda_izq); //ALANTE
-    LATCSET= (1<<rueda_der); //ATRAS
-}
-
-void girarDer(void)
-{
-    LATCSET= (1<<rueda_izq); //ATRAS
-    LATCCLR= (1<<rueda_der); //ALANTE
-}
-
-void pararCoche(void)
-{
-    OC1CON = 0;
-    OC2CON = 0;
-}
-
-void encenderCoche(void)
-{
-    if(OC1CON!=0)
-    {
-        return;
-    }
-    
-    else
-    {
-        OC1CON = 0;
-        OC1R = 500;
-        OC1RS = 500;
-        OC1CON = 0x8006;
-
-        OC2CON = 0;
-        OC2R = 500;
-        OC2RS = 500;
-        OC2CON = 0x8006;
-    }
-}
+#include "UART.h"
+#include "COCHE.h"
 
 int main ( void )
 {
@@ -73,23 +15,67 @@ int main ( void )
     TRISB=0;
     TRISC=0;
     
-    SYSKEY = 0XAA996655;
-    SYSKEY = 0X556699AA;
-    RPC7R = 5;
-    RPC8R = 5;
-    SYSKEY = 0X1CA11CA1;
-    
+   
+    inicializarCoche();
     encenderCoche();
+    inicializarUART(9600); // UART funcionando a 9600 baudios
     
-    T2CON = 0;
-    TMR2 = 0;
-    PR2=500;
-    T2CON = 0x8000;
-    
-    
+    char char_enviado;
     while(1)
-   {
-        avanzar();
-   }
+    {
+        char_enviado=getChar();
+        
+                 if(char_enviado=="w")
+        {
+            avanzar();
+        }
+
+        if(char_enviado=="s")
+        {
+            marchaAtras();
+        }
+
+        if(char_enviado=="a")
+        {
+            girarIzq();
+        }
+
+        if(char_enviado=="d")
+        {
+            girarDer();
+        }
+
+
+        if(char_enviado=='\0')
+        {
+            pararCoche();;    
+        }
+        
+        /*
+        switch(char_enviado)
+        {
+            case "w":
+                avanzar();
+            break;
+
+            case "s":
+                marchaAtras();
+            break;
+
+            case "a":
+                girarIzq();
+            break;
+
+            case "d":
+                girarDer();
+            break;
+
+            default:
+                pararCoche();
+            break;    
+        }
+        */
+             
+    }
          
 }
